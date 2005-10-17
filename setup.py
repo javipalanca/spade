0c7c34b7f704c38d16bd30ac550ef95bd0628a19
Javi Palanca <jpalanca@gmail.com>
2005-10-17 15:45:27 +0000

    setup installer

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SPADE',
+      version='1.9b',
+      description='Smart Python multi-Agent Develpment Environment',
+      author='Javi Palanca, Miguel Escriva, Gustavo Aranda',
+      author_email='jpalanca@dsic.upv.es',
+      url='http://gti-ia.dsic.upv.es/projects/magentix/',
+      package_dir={'spade': 'spade'},
+      packages=['spade'],
+      scripts=['spade-rma.py', 'spade.py'],
+      data_files=[('/etc/spade',['etc/spade.ini','etc/jabber.xml'])]
+     )
