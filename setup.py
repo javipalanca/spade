312eb2eb30c6867f77da0e34bc369dd695d031f2
Miguel Escriva <miesgre@gmail.com>
2005-11-09 13:35:30 +0000

    Windows Installer no host name asked for.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 import os
 from distutils.core import setup
 import glob
+if os.name != "posix":
+	import py2exe
 
 
 if os.name == "posix":
@@ -35,6 +37,20 @@ else:
 	author_email='jpalanca@dsic.upv.es',
 	url='http://gti-ia.dsic.upv.es/projects/magentix/',
 	package_dir={'spade': 'spade'},
-	packages=['spade', 'spade.xmpp']
+	packages=['spade', 'spade.xmpp'],
+	#scripts=['spade-rma.py', 'runspade.py'],
+	console=['spade-rma.py', 'runspade.py','configure.py'],
+	data_files=[
+		('etc',[]),
+		('usr/share/spade',['usr/share/spade/rma.glade']),
+		('.',['readme.txt']),
+		('doc/private',glob.glob('doc/api/private/*')),
+		('doc/public',glob.glob('doc/api/public/*')),
+		('doc',['doc/api/index.html','doc/api/epydoc.css']),
+		('usr/share/spade/jabberd',['usr/share/spade/jabberd/jabberd.exe']),
+		('usr/share/spade/jabberd/libs',glob.glob('usr/share/spade/jabberd/libs/*.dll')),
+		('usr/share/spade/jabberd/spool',[])
+	]
 	)
+	
 	
