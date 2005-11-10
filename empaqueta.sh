a09874eaec4e90e176b88886424a874d4f41946e
Gustavo Aranda <gusarba@gmail.com>
2005-11-10 18:38:01 +0000

    Fixing 'empaqueta.sh'

--- empaqueta.sh
+++ empaqueta.sh
@@ -4,6 +4,7 @@ svn export . tmp
 cd tmp
 rm usr/share/spade/jabberd/*.exe -f
 rm usr/share/spade/jabberd/libs/*.dll -f
+touch usr/share/spade/jabberd/spool/.spool
 makeinstaller
 mv *.package ../setup.bin
 cd ..
