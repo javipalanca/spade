6c92c2594fa0625430d6e416205098ca0f41827d
Javi Palanca <jpalanca@gmail.com>
2006-01-19 12:36:14 +0000

    xmpp and xmppd moved

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.name == "posix":
 	author_email='jpalanca@dsic.upv.es',
 	url='http://gti-ia.dsic.upv.es/projects/magentix/',
 	package_dir={'spade': 'spade'},
-	packages=['spade', 'spade.xmpp', 'spade.xmppd.modules'],
+	packages=['spade', 'spade.xmpp', 'spade.xmppd', 'spade.xmppd.modules'],
 	scripts=['spade-rma.py', 'runspade.py'],
 	data_files=[
 		('/etc/spade',['etc/spade.xml']),
