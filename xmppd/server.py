5de3ddd2a476902356f5ac51d3609fe5095fffc2
Javi Palanca <jpalanca@gmail.com>
2006-01-23 13:05:31 +0000

    packaging

--- xmppd/server.py
+++ xmppd/server.py
@@ -316,7 +316,7 @@ class Session:
             self._session_state=newstate
             if newstate==SESSION_OPENED:
 		self.enqueue(Message(self.peer,Revision,frm=self.ourname))     # Remove in prod. quality server
-		self.DEBUG(str('Sent Welcome message to peer '+str(self.peer)), 'info')
+		self.DEBUG(str('Sent Welcome message to peer '+str(self.peer)), 'sent')
     def set_stream_state(self,newstate):
         if self._stream_state<newstate: self._stream_state=newstate
 
