fdcdb641eb553a76a1e92b4060199ff35c2c2285
Javi Palanca <jpalanca@gmail.com>
2006-01-27 20:20:22 +0000

    packaging

--- spade/Agent.py
+++ spade/Agent.py
@@ -213,7 +213,7 @@ class AbstractAgent(MessageReceiver.MessageReceiver):
                 #Check for queued messages
                 time.sleep(0)
                 proc = False
-                msg = self.blockingReceive(1)
+                msg = self.blockingReceive(0.3)
                 if (msg != None):
                     for b in self._behaviourList:
                         t = self._behaviourList[b]
