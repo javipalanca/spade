8d297163b68e5404f827f2348c375dbfe6a92602
Javi Palanca <jpalanca@gmail.com>
2006-01-31 19:04:32 +0000

    packaging

--- spade/Agent.py
+++ spade/Agent.py
@@ -840,7 +840,7 @@ class Agent(AbstractAgent):
 	self.send(self._msg)
 
 	msg = self.blockingReceive(20)
-	if msg == None or str(msg.getPerformative()) is not u'agree':
+	if msg == None or msg.getPerformative() is not u'agree':
 		print "There was an error registering the Agent. (not agree)" + str(msg.getPerformative())
 		#if debug and msg != None:
 		print str(msg)
