25148805b3ea44aacf60def48542e30b35db0d8d
Javi Palanca <jpalanca@gmail.com>
2006-01-31 16:55:31 +0000

    packaging

--- spade/ACLParser.py
+++ spade/ACLParser.py
@@ -456,7 +456,7 @@ class ACLxmlParser(handler.ContentHandler):
 	def encodeAid(self, aid):
 
 		sb = self.OT + self.AGENT_ID_TAG + self.CT
-		sb = sb + self.encodeTag( self.AGENT_ID_TAG, aid.getName() )
+		sb = sb + self.encodeTag( self.NAME_TAG, None, self.ID_TAG, aid.getName() )
 
 		sb = sb + self.OT + self.ADDRESSES_TAG + self.CT
 		addresses = aid.getAddresses()
