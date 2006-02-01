7c701fc92458ec5d6be1ff9dd85fb00bb3f51477
Javi Palanca <jpalanca@gmail.com>
2006-02-01 20:19:01 +0000

    packaging

--- spade/MessageReceiver.py
+++ spade/MessageReceiver.py
@@ -44,7 +44,7 @@ class MessageReceiver(threading.Thread):
 
 	def __getMessage(self, block, tout):
 		try:
-			message = self.__messages.get(block, tout)
+			message = self.__messages.get(block)#, tout)
 		except Empty:
 			message = None
 			self.kk("VACIO")
@@ -67,7 +67,7 @@ class MessageReceiver(threading.Thread):
 		if time_seg == None waits until a message is received
 		if no message is received returns None
 		"""
-		return self.__getMessage(True) #, time_seg)
+		return self.__getMessage(True, time_seg)
 
 	def postMessage(self, message):
 		if (message != None):
