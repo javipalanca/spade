d3a217bb18197816b416827f6da4a058f1f152db
Javi Palanca <jpalanca@gmail.com>
2006-02-02 14:21:29 +0000

    packaging

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if os.name == "posix":
 		author='Javi Palanca, Miguel Escriva, Gustavo Aranda',
 		author_email='jpalanca@dsic.upv.es',
 		url='http://gti-ia.dsic.upv.es/projects/magentix/',
-		package_dir={'spade': 'spade' , 'munkware':'munkware'},
+		package_dir={'spade': 'spade'},
 		packages=['spade', 'xmpp', 'xmppd', 'tlslite', 'tlslite.utils', 'tlslite.integration', 'munkware'],
 		scripts=['spade-rma.py', 'runspade.py',"tlslite/scripts/tls.py", "tlslite/scripts/tlsdb.py"],
 		data_files=[
