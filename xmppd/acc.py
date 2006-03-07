9b11e909e637727563cb7227254936d539dc39fa
Gustavo Aranda <gusarba@gmail.com>
2006-03-07 00:59:17 +0000

    packaging

--- xmppd/acc.py
+++ xmppd/acc.py
@@ -50,7 +50,7 @@ class accPlugIn(PlugIn):
 			self.DEBUG("ACC PLUGIN: found envelope", "info")
                 	xc = XMLCodec.XMLCodec()
                 	envelope = xc.parse(str(envxml))
-			self.DEBUG("ACC PLUGIN: envelope decoded", "info")
+			self.DEBUG("ACC PLUGIN: envelope decoded: " + str(envelope), "info")
 
                 	if   str(envelope.getAclRepresentation()).lower() == "fipa.acl.rep.string.std":
                         	ac = ACLParser.ACLParser()
