c5dcb375faa59a6ba9e487c769f74ed9c0dda5bb
Javi Palanca <jpalanca@gmail.com>
2006-03-21 11:07:30 +0000

    packaging

--- xmppd/filters/component.py
+++ xmppd/filters/component.py
@@ -10,6 +10,7 @@ class Component(filter.Filter):
 			if stanza.getNamespace()==NS_COMPONENT_ACCEPT and stanza.getName()=='message':
 				self._router.DEBUG("Message for a component",'info')
 				return True
+		self._router.DEBUG("Message NOT for a component",'error')
 		return False
 
 
