64758d6e8c200626cc0e28609bf5495e0f83b09b
Javi Palanca <jpalanca@gmail.com>
2006-03-21 11:48:57 +0000

    packaging

--- xmppd/filters/acc.py
+++ xmppd/filters/acc.py
@@ -1,4 +1,7 @@
 from xmppd import filter
+from xmpp import *
+from spade import *
+from spade.ACLMessage import *
 
 class ACC(filter.Filter):
 
@@ -20,4 +23,55 @@ class ACC(filter.Filter):
 		stanza['to'] = to
 		self._router.DEBUG("retorno stanza","info")
 		return stanza
+
+	def getRealTo(self, mess):
+		"""
+		return the real JID of the receiver of a "jabber:x:fipa" message
+		"""
+		#self._router.DEBUG("ACC PLUGIN: received " + str(dir(mess)), "info")
+		#self.DEBUG("ACC PLUGIN: received " + str(mess), "info")
+		envxml=None
+		receiver_names = []
+		'''
+		payload = None
+		try:
+	            	payload=mess.getBody()
+		except:
+			self.DEBUG("ACC PLUGIN: getBody FAILED. Trying alternate method")
+		try:
+			if payload == None:
+				payload=mess.getTagData['body']
+		except:
+			self.DEBUG("ACC PLUGIN: getTagData FAILED.")
+		'''
+			
+		#self._router.DEBUG("ACC PLUGIN: mess Body got", "info")
+            	children = mess.getChildren()
+		#self._router.DEBUG("ACC PLUGIN: analyzing message structure", "info")
+            	for child in children:
+                	if (child.getNamespace() == "jabber:x:fipa") or (child.getNamespace() == u"jabber:x:fipa"):
+                    		envxml = child.getData()
+            	if (envxml != None):
+			#self._router.DEBUG("ACC PLUGIN: found envelope", "info")
+                	xc = XMLCodec.XMLCodec()
+                	envelope = xc.parse(str(envxml))
+			#self._router.DEBUG("ACC PLUGIN: envelope decoded: " + str(envelope), "info")
+			#self._router.DEBUG("ACC PLUGIN: envelope decoded: " + str(dir(envelope)), "info")
+			for aid in envelope.getTo():
+				receiver_names.append(str(aid.getName()))
+
+                	'''
+			if   str(envelope.getAclRepresentation()).lower() == "fipa.acl.rep.string.std":
+                        	ac = ACLParser.ACLParser()
+                	elif str(envelope.getAclRepresentation()).lower() == "fipa.acl.rep.xml.std":
+                        	ac = ACLParser.ACLxmlParser()
+                	else:
+                        	self.DEBUG("ACC PLUGIN: NO PARSER!", "info")
+			'''
+
+		#ACLmsg = ac.parse(str(payload))	
+		#self.DEBUG("ACC PLUGIN: getRealTo = " + str(ACLmsg.getReceivers()), "info")
+		#for item in ACLmsg.getReceivers():
+		#	receiver_names.append(str(item.getName()))
+		return receiver_names
 
--- xmppd/filters/component.py
+++ xmppd/filters/component.py
@@ -4,15 +4,14 @@ from xmpp  import *
 class Component(filter.Filter):
 
 	def test(self,stanza):
-		self._router.DEBUG("TESTING component",'error')
 		#Counter measure for component originated messages
 		the_from = stanza['from']
 		simple_from = str(the_from)
 		if not('@' in simple_from):  # Component-originated
 			if stanza.getNamespace()==NS_COMPONENT_ACCEPT and stanza.getName()=='message':
-				self._router.DEBUG("Message for a component",'info')
+				#self._router.DEBUG("Message for a component",'info')
 				return True
-		self._router.DEBUG("Message NOT for a component",'error')
+		#self._router.DEBUG("Message NOT for a component",'error')
 		return False
 
 
