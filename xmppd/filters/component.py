5b348d7c73821f37ff0742aa14d6e23a0fa08351
Javi Palanca <jpalanca@gmail.com>
2006-03-21 11:08:50 +0000

    packaging

--- xmppd/filters/component.py
+++ xmppd/filters/component.py
@@ -3,6 +3,7 @@ from xmppd import filter
 class Component(filter.Filter):
 
 	def test(self,stanza):
+		self._router.DEBUG("TESTING component",'error')
 		#Counter measure for component originated messages
 		the_from = stanza['from']
 		simple_from = str(the_from)
