e8bfe9ad6b697d74726b0a28220c5f977f975c30
Javi Palanca <jpalanca@gmail.com>
2006-03-21 12:23:06 +0000

    packaging

--- xmppd/filters/component.py
+++ xmppd/filters/component.py
@@ -18,8 +18,11 @@ class Component(filter.Filter):
 
 	def filter(self,session,stanza):
 
-			to = stanza['to']
-			s=self._router.getsession(to)
+			try:
+				to = stanza['to']
+				s=self._router.getsession(to)
+			except:
+				print "MERDA PA MI"
 			self._router.DEBUG("Component getsession("+str(to)+") ->" + str(s), 'info')
 			if s:
                         	# Fake the namespace. Pose as a client
