21eadf4f2ba19d432809fe8c83517ad6579ae56b
Javi Palanca <jpalanca@gmail.com>
2006-03-27 09:34:56 +0000

    packaging

--- xmppd/filters/component.py
+++ xmppd/filters/component.py
@@ -8,9 +8,9 @@ class Component(filter.Filter):
 
 		filter.Filter.__init__(self,router)
 
-		router.server.Dispatcher.RegisterNamespaceHandler(NS_COMPONENT_ACCEPT,self.routerHandler)
-	        router.server.Dispatcher.RegisterHandler('handshake',self.componentHandler,xmlns=NS_COMPONENT_ACCEPT)
-        	router.server.Dispatcher.RegisterHandler('message',self.router.routerHandler,xmlns=NS_COMPONENT_ACCEPT)
+		router.server.Dispatcher.RegisterNamespaceHandler(NS_COMPONENT_ACCEPT, router.routerHandler)
+	        router.server.Dispatcher.RegisterHandler('handshake', self.componentHandler, xmlns=NS_COMPONENT_ACCEPT)
+        	router.server.Dispatcher.RegisterHandler('message', router.routerHandler, xmlns=NS_COMPONENT_ACCEPT)
 
 
 	def test(self,stanza):
