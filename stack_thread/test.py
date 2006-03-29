26292136ea68796d46b2557578bcdb864fc7074d
Javi Palanca <jpalanca@gmail.com>
2006-03-29 15:32:29 +0000

    threadmodule

--- stack_thread/test.py
+++ stack_thread/test.py
@@ -12,8 +12,12 @@ def run():
 #print thread.start_new_thread(run,())
 #print stack_thread._start_new_thread(run,(),1024)
 #print mymod.snt(run,(), 10204)
-print stack_thread.start_new_thread(run,(),1)
-print "HOLA DESPUES"
+i = 0
+#while 1:
+stack_thread.start_new_thread(run,(),10240000)
+print i
+i = i+1
 
 while 1:
-	time.sleep(1.0)
+	time.sleep(1)
+
