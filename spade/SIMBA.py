076825c214899f55e7969cd1363603307561ed48
Gustavo Aranda <gusarba@gmail.com>
2006-03-30 06:43:54 +0000

    More work in SIMBA integration

--- spade/SIMBA.py
+++ spade/SIMBA.py
@@ -7,10 +7,15 @@ import AID
 import Behaviour
 import ACLParser
 import socket
+import SocketServer
 
+SIMBAPORT = 20001
 
 class SIMBA(Agent.PlatformAgent):
-	class RouteBehaviour(Behaviour.Behaviour):
+	class OutboxBehaviour(Behaviour.Behaviour):
+	'''
+	Behaviour that routes outgoing SIMBA messages
+	'''
 		def __init__(self):
 			Behaviour.Behaviour.__init__(self)
 
@@ -21,7 +26,7 @@ class SIMBA(Agent.PlatformAgent):
 			ip = to.strip("simba://")
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			try:
-				s.connect((ip, 20001))
+				s.connect((ip, SIMBAPORT))
 			except:
 				print "Could not connect to SIMBA socket on " + str(ip)
 			s.send(str(msg))
@@ -34,97 +39,31 @@ class SIMBA(Agent.PlatformAgent):
 				d = {}
 				for to in to_list:
 					self.sendToSimba(msg, to)
-					'''
-					if not to.getAddresses()[0] in d: d[to.getAddresses()[0]]=list()
-					d[to.getAddresses()[0]].append(to)
-				for k,v in d.items():
-					newmsg = msg
-					newmsg.to = v
-					if k[7:] != self.myAgent.getSpadePlatformJID():
-						self.myAgent._sendTo(newmsg, k[7:])
-					else:
-						for recv in v:
-							self.myAgent._sendTo(newmsg, recv.getName())
-					# Reenviamos el msg a todos los destinatarios
-					# Deberiamos de comprobar si el destinatario estaen otro servidor y reenviarselo a la plataforma en vez de a el.
-					# Tambien deberiamos comprobar el protocolo y usar una pasarela en el caso de que sea necesario.
-					#print "Message to", to.getName(), "... Posting!"
-					'''
 			else:
 				print "SIMBA::dying... it shouldn't happen"
 			
+	class InboxBehaviour(Behaviour.Behaviour):
+	'''
+	Behaviour that routes incoming SIMBA messages
+	'''
+		class SimbaRequestHandler(SocketServer.DatagramRequestHandler):
+		'''
+		Request handler for SIMBA messages
+		'''
+			def handle(self):
+				print "SIMBA SS: New incoming message"
+
+		def onStart(self):
+			self.SS = SocketServer.ThreadingUDPServer(("", SIMBAPORT), SimbaRequestHandler)
+			print "SIMBA SS listening on port ", SIMBAPORT
+			self.SS.serve_forever()
+
 	def __init__(self, node, password, server, port):
 		Agent.PlatformAgent.__init__(self, node, password, server, port, debug=[])
 
 	def _setup(self):
-		self.setDefaultBehaviour(self.RouteBehaviour())
+		self.addBehaviour(self.InboxBehaviour(), None)
+		self.setDefaultBehaviour(self.OutboxBehaviour())
 
 
-"""
-class FipperPlatform(threading.Thread):
-    def __init__(self, params=dict()):
-        threading.Thread.__init__(self)
-        self.__params = params
-        self.__mts = MTS(params)
-
-        try:
-            mtp_list = params["mtps"]
-            for mtp in mtp_list:
-                self.__mts.addMTP(mtp)
-        except KeyError:
-            pass
-
-    def getMTS(self):
-        return self.__mts
-        
-    def run(self):
-        pass
-        
-        
-
-
-class MTS:
-
-    def __init__(self, params=dict()):
-        self.__platformagents = dict()
-
-    def addAgent(self, agent):
-        aid = agent.getAID()
-        self.__platformagents[aid] = agent
-
-    def addMTP(self, mtp):
-        pass
-
-     
-    def dispatch(self, msg):
-        print "Dispatching msg..."
-        envelope = msg.getEnvelope()
-        to_list = envelope.getTo()
-        #for to in to_list:
-	#	print to
-	
-        for to in to_list:
-            if to in self.__platformagents:
-                print "Agend finded! Posting..."
-                self.__platformagents[to].postMessage(msg)
-	
-
-
-class MTP:
-    def getName(self):
-        raise NotImplemented
-
-    def getSupportedProtocols(self):
-        raise NotImplemented
-
-    def activate(self, dispatcher, params):
-        raise NotImplemented
-
-    def deactivate(self):
-        raise NotImplemented
-        
-    def deliver(self, addr, envelope, payload):
-        raise NotImplemented
-        
-"""
 
