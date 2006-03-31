b87cc5a09d2b10133f17fc3ba99ca0f332d0b83a
Javi Palanca <jpalanca@gmail.com>
2006-03-31 11:31:28 +0000

    acc supports multiple MTPs

--- xmppd/filters/acc.py
+++ xmppd/filters/acc.py
@@ -19,9 +19,9 @@ class ACC(filter.Filter):
 		parser = SpadeConfigParser.ConfigParser()
 		config = parser.parse(configfile)
 
-		mtps = {}
-		for mtp in config.acc:
-			mtps[mtp.protocol] = mtp.instance(str(mtp))
+		self.mtps = {}
+		for name,mtp in config.acc.items():
+			self.mtps[mtp.protocol] = mtp.instance(name)
 
 	def test(self,stanza):
 
@@ -48,81 +48,46 @@ class ACC(filter.Filter):
 
 	def filter(self,session,stanza):
 
-		mess = stanza
+		xmpp_processed = False
 
 		if (stanza.getError() == None):
 			envxml = self.envelope
 			payload = stanza.getBody()
 			xc = XMLCodec.XMLCodec()
-			envelope = xc.parse(str(envxml))
+			self.envelope = xc.parse(str(envxml))
 
-		
+			for aid in self.envelope.getTo():
+				for addr in aid.getAddresses():
+					protocol = addr.split("://")[0]
+
+					if protocol == "xmpp":
+						#FIXME: only supports 1 xmpp sender
+						receivers = self.getRealTo(stanza)
+						to = str(receivers[0])  # FIX THIS TO ALLOW MULTIPLE RECEIVERS
+						# We do NOT raise the NodeProcessed exception,
+						# the message follows its normal course from here
+						stanza.setTo(to)
+						xmpp_processed = True
 
 
+					elif protocol in self.mtps.keys():
+						mtp = self.mtps[protocol]
+						#envelope is in Envelope format
+						#payload is in string format (with escaped characters)
+						mtp.send(self.envelope,payload)
 
 
+		if xmpp_processed: return stanza
+		else: return None
 
-	"""
 
-		receivers = self.getRealTo(stanza)
-		to = str(receivers[0])  # FIX THIS TO ALLOW MULTIPLE RECEIVERS
-		# We do NOT raise the NodeProcessed exception, the message follows its normal course from here
-		#self._router.DEBUG("ya tengo receiver: "+ to,"info")
-		#stanza['to'] = to
-		stanza.setTo(to)
-		self._router.DEBUG("ACC: retorno stanza con nuevo to: " + str(to),"info")
-		#print str(stanza)
-		return stanza
 
 	def getRealTo(self, mess):
-		
 		#return the real JID of the receiver of a "jabber:x:fipa" message
 		
-		#self._router.DEBUG("ACC PLUGIN: received " + str(dir(mess)), "info")
-		#self.DEBUG("ACC PLUGIN: received " + str(mess), "info")
-		envxml=None
 		receiver_names = []
-		'''
-		payload = None
-		try:
-	            	payload=mess.getBody()
-		except:
-			self.DEBUG("ACC PLUGIN: getBody FAILED. Trying alternate method")
-		try:
-			if payload == None:
-				payload=mess.getTagData['body']
-		except:
-			self.DEBUG("ACC PLUGIN: getTagData FAILED.")
-		'''
-			
-		#self._router.DEBUG("ACC PLUGIN: mess Body got", "info")
-            	children = mess.getChildren()
-		#self._router.DEBUG("ACC PLUGIN: analyzing message structure", "info")
-            	for child in children:
-                	if (child.getNamespace() == "jabber:x:fipa") or (child.getNamespace() == u"jabber:x:fipa"):
-                    		envxml = child.getData()
-            	if (envxml != None):
-			#self._router.DEBUG("ACC PLUGIN: found envelope", "info")
-                	xc = XMLCodec.XMLCodec()
-                	envelope = xc.parse(str(envxml))
-			#self._router.DEBUG("ACC PLUGIN: envelope decoded: " + str(envelope), "info")
-			#self._router.DEBUG("ACC PLUGIN: envelope decoded: " + str(dir(envelope)), "info")
-			for aid in envelope.getTo():
-				receiver_names.append(str(aid.getName()))
+		for aid in self.envelope.getTo():
+			receiver_names.append(str(aid.getName()))
 
-                	'''
-			if   str(envelope.getAclRepresentation()).lower() == "fipa.acl.rep.string.std":
-                        	ac = ACLParser.ACLParser()
-                	elif str(envelope.getAclRepresentation()).lower() == "fipa.acl.rep.xml.std":
-                        	ac = ACLParser.ACLxmlParser()
-                	else:
-                        	self.DEBUG("ACC PLUGIN: NO PARSER!", "info")
-			'''
-
-		#ACLmsg = ac.parse(str(payload))	
-		#self.DEBUG("ACC PLUGIN: getRealTo = " + str(ACLmsg.getReceivers()), "info")
-		#for item in ACLmsg.getReceivers():
-		#	receiver_names.append(str(item.getName()))
 		return receiver_names
-	"""
 
