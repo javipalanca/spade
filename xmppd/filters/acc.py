3bb3b86981715b34ac9a1513071bd90a60db95d6
Javi Palanca <jpalanca@gmail.com>
2006-04-04 11:29:12 +0000

    packaging

--- xmppd/filters/acc.py
+++ xmppd/filters/acc.py
@@ -26,7 +26,7 @@ class ACC(filter.Filter):
 		for name,mtp in config.acc.items():
 			#self.mtps[mtp.protocol] = mtp.instance(name)
 			try:
-				mod = __import__(path + os.sep + "mtp" + os.sep + name)
+				mod = __import__(str(path + os.sep + "mtp" + os.sep + name))
 			except:
 				print "PETO AL IMPORTAR " + str(path + os.sep + "mtp" + os.sep + name)
 			try:
