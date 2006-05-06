eae708b25908ee2790393c63db90149fba743ffb
Gustavo Aranda <gusarba@gmail.com>
2006-05-06 15:13:10 +0000

    packaging

--- xmppd/roster.py
+++ xmppd/roster.py
@@ -1,7 +1,7 @@
 from xmpp import *
 from spade import *
 from spade.ACLMessage import *
-
+import types
 
 class rosterPlugIn(PlugIn):
         #NS='jabber:x:fipa'
@@ -9,7 +9,40 @@ class rosterPlugIn(PlugIn):
 	def plugin(self, server):
 		server.Dispatcher.RegisterHandler('iq', self.rosterHandler, ns=NS_ROSTER)
 		server.Dispatcher.RegisterHandler('iq', self.rosterHandler, ns=u'jabber:iq:roster')
+
+		# Initialize the roster management system
+		self.rosters = {}
+
 		self.DEBUG("Roster manager loaded", "warn")
+
+	"""
+	def createRoster(self, jid):
+		'''Create a roster for a given jid'''
+		if type(jid) == types.InstanceType:
+			# Transform jid to string representation (faster?)
+			jid = jid.getStripped()	
+		if jid not in self.rosters.keys():
+			self.rosters[jid] = []
+	"""
+
+	
+	def getRoster(self, jid):
+		if type(jid) == types.InstanceType:
+			# Transform jid to string representation (faster?)
+			jid = jid.getStripped()
+		if jid not in self.rosters.keys()
+			# Particular rosters are lists
+			self.rosters[jid] = {}
+		return self.rosters[jid]
+
+
+	"""
+	MyRoster:
+	{ friend@localhost: { name:'Friend', subscription: 'none' }
+	  another@localhost: { }
+	  . . . 
+	}
+	"""
 
         def rosterHandler(self, session, stanza):
                 print "####################################"
@@ -30,11 +63,19 @@ class rosterPlugIn(PlugIn):
 		                print "####################################"
 				iq = Iq('result', NS_ROSTER)
 				query=iq.getTag('query')
-				attrs={'jid':'friend@localhost'}
-				item=query.setTag('item',attrs)
+				# Fill the roster
+				ros = self.getRoster(stanza.getAttr('from'))
+				for key, value in ros:
+					attrs={'jid':key}
+					if 'name' in value.keys():
+						attrs['name'] = value['name']
+					if 'subscription' in value.keys():
+						attrs['subscription'] = value['name']						
+					item=query.setTag('item',attrs)
 				session.enqueue(iq)	
                 		print "####################################"
-				print "THE ROSTER WAS SENT"
+				print "THE ROSTER WAS SENT:"
+				print str(iq)
 		                print "####################################"
 		
 
