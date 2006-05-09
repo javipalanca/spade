bb3adde70f38d74acd7d4cfa3dc21ca9caa61628
Gustavo Aranda <gusarba@gmail.com>
2006-05-09 16:20:12 +0000

    packaging

--- xmppd/router.py
+++ xmppd/router.py
@@ -99,6 +99,9 @@ class Router(PlugIn):
 	elif typ=='subscribed':
 	    # Presence Subscription confirmation
 	    self.DEBUG("Presence subscription confirmation found from "+barejid+" to "+str(to), "warn")
+
+	    raise NodeProcessed  # TEMPORARILY
+
 	    # Modify sender's roster to reflect the subscription confirmation
 	    try:
 	    	self.server.rosterPlugIn.makeSubscription(str(to), barejid, session, subs='to')
