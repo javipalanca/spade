0eb12382c0fbfc50093fc991050885a019cb14df
Gustavo Aranda <gusarba@gmail.com>
2006-05-16 11:22:34 +0000

    Started working in MUC module for server

---
+++ spade/muc.py
@@ -0,0 +1,221 @@
+import xmpp *
+
+
+class Participant:
+	"""
+	A client that participates in a MUC room
+	"""
+	def __init__(self, fulljid, barejid=None, nick=None, role=None, affiliation=None):
+		# Get data from constructor
+		self.fulljid = fulljid
+		# If there was no barejid, build it
+		if not barejid:
+			 self.barejid = str(fulljid).split('/')[0]
+		# If there was no nick, take the barejid instead
+		if not nick:
+			self.nick = self.barejid
+		# If there was no role, take the 'none' role
+		if not role:
+			self.role = 'none'
+		# Same as role
+		if not affiliation:
+			self.affiliation = 'none'
+
+	def __str__(self):
+		print '<' + str(self.fulljid) + ' barejid="' + self.barejid + '" nick="' + self.nick + '" role="' + self.role + '" affiliation="' + self.affiliation + '">'
+
+	def getFullJID(self):
+		"""
+		Get the participant's full JID
+		"""
+		return self.fulljid
+
+	def getBareJID(self):
+		"""
+		Get the participant's bare JID
+		"""
+		return self.barejid
+
+	def getNick(self):
+		"""
+		Get the participant's nickname
+		"""
+		return self.nick
+
+	def getRole(self):
+		"""
+		Get the participant's role
+		"""
+		return self.role
+
+	def getAffiliation(self):
+		"""
+		Get the participant's affiliation
+		"""
+		return self.affiliation
+
+	def setFullJID(self, fulljid):
+		"""
+		Set the participant's full JID
+		"""
+		self.fulljid = fulljid
+
+	def setBareJID(self, barejid):
+		"""
+		Set the participant's bare JID
+		"""
+		self.barejid = barejid
+
+	def setNick(self, nick):
+		"""
+		Set the participant's nickname
+		"""
+		self.nick = nick
+
+	def setRole(self, role):
+		"""
+		Set the participant's role
+		"""
+		self.role = role
+
+	def setAffiliation(self, affiliation):
+		"""
+		Set the participant's affiliation
+		"""
+		self.affiliation = affiliation
+
+
+class Room:
+	"""
+	A MUC room
+	"""
+	def __init__(self, jid, subject=None, template=None, creator=None, whitelist=[], blacklist=[], password=None):
+		# Initialize default room specific values
+		self.hidden = False
+		self.open = True
+		self.moderated = False
+		self.semi_anonymous = False
+		self.unsecured = True
+		self.password = password
+		self.persistent = False
+
+		# Initialize default role privileges
+		# Every privilege is expressed in a hierarchical form.
+		# If a given privilege is granted to a client with the role of 'visitor',
+		# all superior roles, will have that privilege too (i.e. 'participant' and 'moderator')
+		self.role_privileges = {}
+		self.role_privileges['change_nick'] = 'visitor'
+		self.role_privileges['send_private'] = 'visitor'
+		self.role_privileges['invite'] = 'visitor'
+		self.role_privileges['send'] = 'participant'
+		self.role_privileges['subject'] = 'participant'
+
+		# Get data from constructor
+		self.jid = jid
+		# If there was no subject, take the first part of the jid instead
+		if not subject:
+			self.subject = jid.split('@')[0]
+		# If there was a template, change values by default
+		if template:
+			print "TODO: Implement room templates"
+		# Initialize participants dict. It will be indexed by the participants full JIDs
+		self.participants = {}
+		# If there was a creator, add it to the participants dict
+		if creator:
+			self.creator = creator
+			creator.setRole('moderator')
+			self.participants[creator.getFullJID()] = creator
+
+		# Initialize white and blacklists
+		self.whitelist = whitelist
+		self.blacklist = blacklist
+
+	def __str__(self):
+		print "Room: ", str(self.jid), self.subject
+		print "Hidden = ", str(self.hidden), "Open = ", str(self.open), "Moderated = ", str(self.moderated), "Semi-Anonymous = ", str(self.semi_anonymous), "Unsecured = ", str(self.unsecured), "Persistent = ", str(self.persistent)
+		print "Role Privileges = ", str(self.role_privileges)
+		print "Participants = ", str(self.participants.keys())
+		print "Creator = ", str(self.creator.getFullJID())
+		print "Whitelist = ", str(self.whitelist)
+		print "Blacklist = ", str(self.blacklist)
+
+	def addParticipant(self, fulljid=None, barejid=None, nick=None, role=None, affiliation=None, participant=None, password=None):
+		"""
+		Add a participant to a room
+		"""
+		# If a participant object has been passed here, copy all its attributes into the new participant
+		if participant:
+			# Copy another object
+			p = Participant(participant.getFullJID, participant.getBareJid(), participant.getNick(), participant.getRole(), participant.getAffiliation())
+		elif fulljid not in self.participants.keys():
+			# Instantiate a new participant
+			p = Participant(fulljid)
+			# Now, override the new participant's attributes
+			p.setNick(nick)
+			p.setRole(role)
+			p.setAffiliation(affiliation)
+		else:
+			# Modify an existing participant
+			p = self.participants[fulljid]
+
+		# See wether the participant can be added to the room depending on the room's type
+		# Case 0: The participant is blacklisted
+		if p.getBareJid() in self.blacklist:
+			# "I told you not to come back! Get the f**k out of here!"
+			return False
+		# Case 1: Open and without password. Free way
+		if self.open and self.unsecured:
+			# Set the participants role based on the room's moderation and the 'role' parameter provided
+			if self.moderated and not role:
+				p.setRole('visitor')
+			elif not self.moderated and not role:
+				p.setRole('participant')
+			self.participants[p.getFullJID()] = p
+			return True
+		# Case 2: Open but with password. "Say say say say the password"
+		elif self.open and not self.unsecured:
+			if password == self.password:
+				# Free way
+				# Set the participants role based on the room's moderation and the 'role' parameter provided
+				if self.moderated and not role:
+					p.setRole('visitor')
+				elif not self.moderated and not role:
+					p.setRole('participant')
+				self.participants[p.getFullJID()] = p
+				return True
+			else:
+				# Wrong password
+				return False
+		# Case 3: Members-only but without password
+		elif not self.open and self.unsecured:
+			if p.getBareJid() in self.whitelist:
+				# "Welcome back, sir"
+				# Set the participants role based on the room's moderation and the 'role' parameter provided
+				if self.moderated and not role:
+					p.setRole('visitor')
+				elif not self.moderated and not role:
+					p.setRole('participant')
+				self.participants[p.getFullJID()] = p
+				return True
+			else:
+				# "You're not on my list. Get lost"
+				return False
+		# Case 4: Members-only and with password
+		elif not self.open and not self.unsecured:
+			if p.getBareJid() in self.whitelist:
+				if password == self.password:
+					# Free way
+					# Set the participants role based on the room's moderation and the 'role' parameter provided
+					if self.moderated and not role:
+						p.setRole('visitor')
+					elif not self.moderated and not role:
+						p.setRole('participant')
+					self.participants[p.getFullJID()] = p
+					return True
+				else:
+					# Bad password
+					return False
+			else:
+				# Not a member. Get lost
+				return False
+			
