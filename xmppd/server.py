b13ea9fb2b21c5359d9fa5a51df50aa87dc3edff
Gustavo Aranda <gusarba@gmail.com>
2006-06-06 11:12:00 +0000

    packaging

--- xmppd/server.py
+++ xmppd/server.py
@@ -155,8 +155,7 @@ class Session:
 
     def receive(self):
         """Reads all pending incoming data. Raises IOError on disconnect."""
-        ###try: received = self._recv(10240)
-        try: received = self._recv(128)
+        try: received = self._recv(10240)
         except: received = ''
 
         if len(received): # length of 0 means disconnect
