9e5ac3641524812ffdaa52971c10b675790c4339
Gustavo Aranda <gusarba@gmail.com>
2007-06-27 15:07:38 +0000

    Testing trac changes. closes #9

--- spade/Agent.py
+++ spade/Agent.py
@@ -1520,7 +1520,7 @@ class AbstractAgent(MessageReceiver.MessageReceiver):
     	the service template is a DfAgentDescriptor
         
     	"""
-        a=8
+        a=9
         msg = ACLMessage.ACLMessage()
         template = Behaviour.ACLTemplate()
         template.setConversationId(msg.getConversationId())
