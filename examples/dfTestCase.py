5d57d1b1836d3bea29d6a480fa07ee39b79086f6
Gustavo Aranda <gusarba@gmail.com>
2009-05-26 11:18:59 +0000

    DF unit tests finished (for the moment)

--- examples/dfTestCase.py
+++ examples/dfTestCase.py
@@ -165,10 +165,13 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(len(self.a.result), 1)
         
         self.assertEqual(self.a.result[0].getName(), self.a.getAID())
-        self.assertEqual(len(self.a.result[0].getServices()), 1)
+        self.assertEqual(len(self.a.result[0].getServices()), 2)
         
-        self.assertEqual(self.a.result[0].getServices()[0].getType(),"unittest_type_1_modified")
-        
+        if self.a.result[0].getServices()[0].getName() not in ['unittest_name_1','unittest_name_2']:
+            self.fail()
+        if self.a.result[0].getServices()[1].getName() not in ['unittest_name_1','unittest_name_2']:
+            self.fail()
+               
         self.a.result = None
         
         #modify service
@@ -187,10 +190,10 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(len(self.a.result), 1)
         
         self.assertEqual(self.a.result[0].getName(), self.a.getAID())
-        self.assertEqual(len(self.a.result[0].getServices()), 2)
-        
-        if self.a.result[0].getServices()[0].getName() not in ['unittest_name_1','unittest_name_2']:
-            self.fail()
+
+        self.assertEqual(len(self.a.result[0].getServices()), 1)
+
+        self.assertEqual(self.a.result[0].getServices()[0].getType(),"unittest_type_1_modified")
         
         self.a.result = None
             
@@ -211,41 +214,122 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(self.a.result, [])
         
 
-    """    
+        
     def testSearchNotPresent(self):
-        self.b.stop()
-        for agent in ["notpresent","b"]:
-            self.a.addBehaviour(SearchBehav(agent), None)
-            counter = 0
-            while self.a.search == None and counter < 20:
-                time.sleep(1)
-                counter +=1
+        #check service is not registered
+        self.a.addBehaviour(SearchBehav("a"),None)
+        self.waitfor(self.a.result)
 
-            self.assertEqual(len(self.a.search), 0)
-            
-    def testModifyAllowed(self):
+        self.assertNotEqual(self.a.result, None)    
+        self.assertEqual(len(self.a.result), 0)
 
-        self.a.addBehaviour(ModifyBehav("a"), None)
-        counter = 0
-        while self.a.search == None and counter < 20:
-            time.sleep(1)
-            counter +=1
+        self.a.result = None
+                 
+        #register service
+        self.a.addBehaviour(RegisterBehav("b"),None)
+        self.waitfor(self.a.result)
 
         self.assertEqual(self.a.result, True)
-        self.assertEqual(self.a.search[0]["ownership"], "UNITTEST")
+
+        self.a.result = None
+
+        #check service is not registered
+        self.a.addBehaviour(SearchBehav("a"),None)
+        self.waitfor(self.a.result)
+
+        self.assertNotEqual(self.a.result, None)    
+        self.assertEqual(len(self.a.result), 0)
+         
+        self.a.result = None
+
+        #deregister service
+        self.a.addBehaviour(DeRegisterBehav("a"),None)
+        self.waitfor(self.a.result)
+
+        self.assertEqual(self.a.result, True)
+
+        #check service is deregistered
+        self.a.result = False
+        self.a.addBehaviour(SearchBehav("a"),None)
+        counter = 1
+        while self.a.result == False and counter < 20:
+             time.sleep(1)
+             counter += 1
+
+        self.assertEqual(self.a.result, [])
+
+
 
     def testModifyNotAllowed(self):
-
-        self.a.addBehaviour(ModifyBehav("b"), None)
-        counter = 0
-        while self.a.search == None and counter < 20:
-            time.sleep(1)
-            counter +=1
-
-        self.assertEqual(self.a.result, False)
-        self.assertNotEqual(self.a.search[0]["ownership"], "UNITTEST")
+        #register service
+        self.a.addBehaviour(RegisterBehav("a"),None)
+        self.waitfor(self.a.result)
+            
+        self.assertEqual(self.a.result, True)
         
-    """
+        self.a.result = None
+        
+        #check service is registered
+        self.a.addBehaviour(SearchBehav("a"),None)
+        self.waitfor(self.a.result)
+        
+        self.assertNotEqual(self.a.result, None)    
+        self.assertEqual(len(self.a.result), 1)
+        
+        self.assertEqual(self.a.result[0].getName(), self.a.getAID())
+        self.assertEqual(len(self.a.result[0].getServices()), 2)
+        
+        if self.a.result[0].getServices()[0].getName() not in ['unittest_name_1','unittest_name_2']:
+            self.fail()
+        if self.a.result[0].getServices()[1].getName() not in ['unittest_name_1','unittest_name_2']:
+            self.fail()
+               
+        self.a.result = None
+        self.b.result = None
+        
+        #modify service
+        self.b.addBehaviour(ModifyBehav("a"),None)
+        self.waitfor(self.b.result)
+        
+        self.assertEqual(self.b.result, False)
+        
+        self.b.result = None
+                
+        #check service is NOT modified
+        self.a.addBehaviour(SearchBehav("a"),None)
+        self.waitfor(self.a.result)
+        
+        self.assertNotEqual(self.a.result, None)    
+        self.assertEqual(len(self.a.result), 1)
+        
+        self.assertEqual(self.a.result[0].getName(), self.a.getAID())
+
+        self.assertEqual(len(self.a.result[0].getServices()), 2)
+
+        if self.a.result[0].getServices()[0].getName() not in ['unittest_name_1','unittest_name_2']:
+            self.fail()
+        if self.a.result[0].getServices()[1].getName() not in ['unittest_name_1','unittest_name_2']:
+            self.fail()
+        
+        self.a.result = None
+            
+        #deregister service
+        self.a.addBehaviour(DeRegisterBehav("a"),None)
+        self.waitfor(self.a.result)
+            
+        self.assertEqual(self.a.result, True)
+        
+        #check service is deregistered
+        self.a.result = False
+        self.a.addBehaviour(SearchBehav("a"),None)
+        counter = 1
+        while self.a.result == False and counter < 20:
+            time.sleep(1)
+            counter += 1
+        
+        self.assertEqual(self.a.result, [])
+
+
 
 
 if __name__ == "__main__":
