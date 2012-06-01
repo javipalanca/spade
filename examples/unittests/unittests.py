ef25cd6a05bb9477c526b0b300ec819c0a12a8ae
Javi Palanca <jpalanca@gmail.com>
2012-06-01 16:52:01 +0000

    added unittest-xml-reporting

--- examples/unittests/unittests.py
+++ examples/unittests/unittests.py
@@ -8,4 +8,15 @@ from aidTestCase   import *
 from dadTestCase   import *
 from eventbehavTestCase import *
 from pubsubTestCase import *
-unittest.main()
+
+try:
+	import xmlrunner
+	use_xmlrunner = True
+except:
+	use_xmlrunner = False
+
+if use_xmlrunner:
+	unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+else:
+	print "XMLTestRunner not found."
+	unittest.main()
