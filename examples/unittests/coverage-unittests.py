9071ae39938ffb3eab2a80ca792c391be4932e2f
Javi Palanca <jpalanca@gmail.com>
2012-06-01 18:05:20 +0000

    added unittests for coverage analysis

---
+++ examples/unittests/coverage-unittests.py
@@ -0,0 +1,12 @@
+from basicTestCase import *
+from amsTestCase   import *
+from dfTestCase    import *
+from coTestCase    import *
+from p2pTestCase   import *
+from rpcTestCase   import *
+from aidTestCase   import *
+from dadTestCase   import *
+from eventbehavTestCase import *
+from pubsubTestCase import *
+
+unittest.main()
