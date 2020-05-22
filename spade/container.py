5b8c61858e866f62206c1e87d142bcb80ce544b7
jpalanca <jpalanca@dsic.upv.es>
2020-05-22 17:56:48 +0200

    ignore version for coverage.

--- spade/container.py
+++ spade/container.py
@@ -128,7 +128,7 @@ class AioThread(Thread):
         try:
             self.loop.run_forever()
             if sys.version_info >= (3, 7):
-                tasks = asyncio.all_tasks(loop=self.loop)
+                tasks = asyncio.all_tasks(loop=self.loop)  # pragma: no cover
             else:
                 tasks = asyncio.Task.all_tasks(loop=self.loop)  # pragma: no cover
             for task in tasks:
