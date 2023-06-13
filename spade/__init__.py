22458896ccd86c4191470a29f597a76527b9a733
javipalanca <jpalanca@gmail.com>
2023-06-13 12:33:30 +0200

    Bump version: 3.2.3 → 3.3.0

--- spade/__init__.py
+++ spade/__init__.py
@@ -12,7 +12,7 @@ from .container import run_container as run  # noqa: F401
 
 __author__ = """Javi Palanca"""
 __email__ = "jpalanca@gmail.com"
-__version__ = "3.2.3"
+__version__ = "3.3.0"
 
 __all__ = ["agent", "behaviour", "message", "template"]
 
